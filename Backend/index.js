b95838eef52d445ccb720ab5477d0b8ca1510602
Abhishek Yadav <abhishekpsitfor4years@gmail.com>
2024-02-16 08:56:05 +0530

    fixed bug

--- Backend/index.js
+++ Backend/index.js
@@ -24,17 +24,35 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+const connectedUsers = [];
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("addUser", (username) => {
+    connectedUsers.push({ id: socket.id, username });
+    io.emit(
+      "users",
+      connectedUsers.map((user) => user.username)
+    );
+  });
 
   socket.on("chatMessage", (data) => {
     const { username, message } = data;
-
+    console.log(`Message from ${username}: ${message}`);
     io.emit("chatMessage", { username, message });
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log(`User disconnected: ${socket.id}`);
+
+    const index = connectedUsers.findIndex((user) => user.id === socket.id);
+    if (index !== -1) {
+      connectedUsers.splice(index, 1);
+      io.emit(
+        "users",
+        connectedUsers.map((user) => user.username)
+      );
+    }
   });
 });
 
